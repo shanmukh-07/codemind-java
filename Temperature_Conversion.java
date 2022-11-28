84d803fe575699f9484570fc142b0044a27ce6b1
shanmukh-07 <97872565+shanmukh-07@users.noreply.github.com>
2022-11-28 22:43:18 +0530

    import java.util.Scanner; class Shanmukh {     public static void main(String args[])     {         Scanner sc = new Scanner(System.in);         int c=sc.nextInt();         double s=32+(c*1.8);         System.out.format("%.2f",s);     } }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Shanmukh
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int c=sc.nextInt();
+        double s=32+(c*1.8);
+        System.out.format("%.2f",s);
+    }
+}
