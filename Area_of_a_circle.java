01184d9f2ffa3485677c2d484d5f062208f94cda
shanmukh-07 <97872565+shanmukh-07@users.noreply.github.com>
2022-11-28 22:51:09 +0530

    import java.util.Scanner; class Shanmukh {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int r=sc.nextInt();         double ar=3.14*r*r;         System.out.format("%.2f",ar);     } }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Shanmukh
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int r=sc.nextInt();
+        double ar=3.14*r*r;
+        System.out.format("%.2f",ar);
+    }
+}
