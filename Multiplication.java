878105b0bfcb626202713ae804c8a66fc779c2a1
shanmukh-07 <97872565+shanmukh-07@users.noreply.github.com>
2022-11-29 22:58:04 +0530

    import java.util.Scanner; class Shan {     public static void main(String args[])     {         Scanner m=new Scanner(System.in);         int a=m.nextInt();         int b=m.nextInt();         int n=a*b;         System.out.println(n);     } }

---
+++ Multiplication.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Shan
+{
+    public static void main(String args[])
+    {
+        Scanner m=new Scanner(System.in);
+        int a=m.nextInt();
+        int b=m.nextInt();
+        int n=a*b;
+        System.out.println(n);
+    }
+}
