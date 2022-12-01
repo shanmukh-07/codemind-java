372982601b3c34e8e10ea4063943944e3040a580
shanmukh-07 <97872565+shanmukh-07@users.noreply.github.com>
2022-12-01 22:23:17 +0530

    import java.util.Scanner; class Shan {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         float a=sc.nextFloat();         float b=sc.nextFloat();         float c=a*b;         System.out.format("%.2f",c);     } }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Shan
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        float a=sc.nextFloat();
+        float b=sc.nextFloat();
+        float c=a*b;
+        System.out.format("%.2f",c);
+    }
+}
