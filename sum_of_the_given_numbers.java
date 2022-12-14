ca7af8a6e66659387bfe150f8a7bc263d98e7a5a
shanmukh-07 <97872565+shanmukh-07@users.noreply.github.com>
2022-12-14 16:02:29 +0530

    import java.util.Scanner; class SUm14 { public static void main(String args[]) { Scanner Sc=new Scanner(System.in); int a=Sc.nextInt(); for(int i=0;i<=a;i++) { int b=Sc.nextInt(); int c=Sc.nextInt(); int s=b+c; System.out.println(s); } } }

---
+++ sum_of_the_given_numbers.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class SUm14
+{
+public static void main(String args[])
+{
+Scanner Sc=new Scanner(System.in);
+int a=Sc.nextInt();
+for(int i=0;i<=a;i++)
+{
+int b=Sc.nextInt();
+int c=Sc.nextInt();
+int s=b+c;
+System.out.println(s);
+}
+}
+}
