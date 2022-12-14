a2b86849d39b9d48644003036f3e0c7e443f2aeb
shanmukh-07 <97872565+shanmukh-07@users.noreply.github.com>
2022-12-14 23:09:38 +0530

    import java.util.Scanner; class Lower{     public static void main(String args[]){         Scanner s=new Scanner(System.in);         String a=s.nextLine();         System.out.print(a.toLowerCase());     } }

---
+++ To_Lower_Case.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class Lower{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        String a=s.nextLine();
+        System.out.print(a.toLowerCase());
+    }
+}
