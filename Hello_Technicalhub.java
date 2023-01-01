c5372802e661865f55fb235c0c4d70a48fc742d8
shanmukh-07 <97872565+shanmukh-07@users.noreply.github.com>
2023-01-01 11:45:22 +0530

    import java.util.Scanner; class Shan{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         String a;         System.out.println("Hello Technicalhub");         a=sc.nextLine();         System.out.println(a);     } }

---
+++ Hello_Technicalhub.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Shan{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String a;
+        System.out.println("Hello Technicalhub");
+        a=sc.nextLine();
+        System.out.println(a);
+    }
+}
